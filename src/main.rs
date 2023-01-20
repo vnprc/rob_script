ed63a169bc1d19076581625ffcc3504131487215
Evan Price <vnprc@protonmail.com>
2023-01-20 03:34:48 -0500

    write a human readable json policy tree

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate serde_json;
 extern crate serde;
 extern crate bitcoin;
 
+use std::fmt::Display;
 use std::fs::File;
 use std::str::FromStr;
 use std::io::prelude::*;
@@ -13,7 +14,7 @@ use bdk::descriptor::Segwitv0;
 use bdk::{wallet::AddressIndex, Error, KeychainKind, Wallet, SyncOptions};
 use bdk::blockchain::{ElectrumBlockchain};
 use bdk::{Balance, TransactionDetails};
-use bdk::descriptor::policy::Policy;
+use bdk::descriptor::policy::{Policy, SatisfiableItem, PkOrF};
 use bdk::miniscript::Terminal;
 use serde::{Serialize, Deserialize};
 use serde_json::json;
@@ -51,17 +52,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn recursive_branch(node: &Miniscript<String, Segwitv0>, depth: u32) -> serde_json::Value {
-    let mut child_json = json!([]);
-    for branch in node.branches() {
-        child_json.as_array_mut().unwrap().push(recursive_branch(&branch, depth + 1));
-    }
-    
-    let mut json = print_readable_policy(&node.node, depth);
+fn get_policy_descriptions(policy: Policy, depth: u32) -> serde_json::Value {
+    let children: Option<serde_json::Value> = match &policy.item {
+        SatisfiableItem::Thresh {items, threshold: _} => {
+            let mut policy_descriptions:Vec<serde_json::Value> = Vec::new();
+            for item in items {
+                policy_descriptions.push(get_policy_descriptions(item.clone(), depth + 1));
+            }
+            Some(json!(policy_descriptions))
+        }
+        _ => None,
+    };
 
-    if child_json != json!([]) {
-            json["zchildren"] = child_json;
+    let mut json = json!({
+        "description": description(policy),
+        "depth": depth,
+    });
+
+    // include child policies if they exist
+    if children.is_some() {
+        json = match json {
+            serde_json::Value::Object(m) => {
+                let mut m = m.clone();
+                m.insert(String::from("items"), children.unwrap());
+                serde_json::Value::Object(m)
+            },
+            v => v.clone(),
+        };
     }
+
     json
 }
 
@@ -95,15 +114,17 @@ fn generate_output_files(wallet: &Wallet<MemoryDatabase>, segwit_policy: &Minisc
         addresses.push(wallet.get_address(AddressIndex::New).expect("error retrieving next address").to_string())
     });
 
+    let extern_policies = wallet.policies(KeychainKind::External).expect("error retrieving external policies").unwrap();
+
     let json_output = Output {
         balance: &wallet.get_balance().expect("error retrieving balance"),
         transactions: &wallet.list_transactions(true).expect("error retrieving transactions"),
-        extern_policies: &wallet.policies(KeychainKind::External).expect("error retrieving external policies").unwrap(),
+        extern_policies: &extern_policies.clone(),
         intern_policies: &wallet.policies(KeychainKind::Internal).expect("error retrieving internal policies").unwrap(),
         addresses: addresses,
         extern_descriptor: &wallet.public_descriptor(KeychainKind::External).expect("error retrieving external descriptor").unwrap(),
         intern_descriptor: &wallet.public_descriptor(KeychainKind::Internal).expect("error retrieving internal descriptor").unwrap(),
-        policy_structure: &recursive_branch(&segwit_policy, 0),
+        policy_structure: &get_policy_descriptions(extern_policies, 0),
     };
 
     let mut file = File::create(OUTPUT_FILE)?;
@@ -130,36 +151,52 @@ struct Addresses {
     addresses: Vec<String>,
 }
 
-fn print_readable_policy(node: &Terminal<String, Segwitv0>, depth: u32) -> serde_json::Value {
-    match node {
-        Terminal::True => json!({"name": "True", "depth": depth, "depth": depth}),
-        Terminal::False => json!({"name": "False", "depth": depth}),
-        Terminal::PkK(pk) => json!({"name": "PkK", "value": pk, "depth": depth}),
-        Terminal::PkH(pk) => json!({"name": "PkH", "value": pk, "depth": depth}),
-        Terminal::RawPkH(hash) => json!({"name": "RawPkH", "value": hash, "depth": depth}),
-        Terminal::After(locktime) => json!({"name": "After", "value": locktime, "depth": depth}),
-        Terminal::Older(sequence) => json!({"name": "Older", "value": sequence, "depth": depth}),
-        Terminal::Sha256(hash) => json!({"name": "Sha256", "value": hash, "depth": depth}),
-        Terminal::Hash256(hash) => json!({"name": "Hash256", "value": hash, "depth": depth}),
-        Terminal::Ripemd160(hash) => json!({"name": "Ripemd160", "value": hash, "depth": depth}),
-        Terminal::Hash160(hash) => json!({"name": "Hash160", "value": hash, "depth": depth}),
-        Terminal::Alt(_miniscript) => json!({"name": "Alt", "depth": depth}),
-        Terminal::Swap(_miniscript) => json!({"name": "Swap", "depth": depth}),
-        Terminal::Check(_miniscript) => json!({"name": "Check", "depth": depth}),
-        Terminal::DupIf(_miniscript) => json!({"name": "DupIf", "depth": depth}),
-        Terminal::Verify(_miniscript) => json!({"name": "Verify", "depth": depth}),
-        Terminal::NonZero(_miniscript) => json!({"name": "NonZero", "depth": depth}),
-        Terminal::ZeroNotEqual(_miniscript) => json!({"name": "ZeroNotEqual", "depth": depth}),
-        Terminal::AndV(_miniscript1, _miniscript2) => json!({"name": "AndV", "depth": depth}),
-        Terminal::AndB(_miniscript1, _miniscript2) => json!({"name": "AndB", "depth": depth}),
-        Terminal::AndOr(_miniscript1, _miniscript2, _miniscript3) => json!({"name": "AndOr", "depth": depth}),
-        Terminal::OrB(_miniscript1, _miniscript2) => json!({"name": "OrB", "depth": depth}),
-        Terminal::OrD(_miniscript1, _miniscript2) => json!({"name": "OrD", "depth": depth}),
-        Terminal::OrC(_miniscript1, _miniscript2) => json!({"name": "OrC", "depth": depth}),
-        Terminal::OrI(_miniscript1, _miniscript2) => json!({"name": "OrI", "depth": depth}),
-        Terminal::Thresh(m, _miniscripts) => json!({"m": m, "name": "Thresh"}),
-        Terminal::Multi(m, miniscript) => json!({"m": m, "name": "Multi", "value": miniscript}),
-        Terminal::MultiA(m, miniscript) => json!({"m": m, "name": "MultiA", "value": miniscript}),
-        _ => json!({"name": "Unmatched"})
+pub fn description(policy: Policy) -> String {
+    match &policy.item {
+        SatisfiableItem::EcdsaSignature(key) => format!("ECDSA Sig of {}", display_key(key)),
+        SatisfiableItem::SchnorrSignature(key) => {
+            format!("Schnorr Sig of {}", display_key(key))
+        }
+        SatisfiableItem::Sha256Preimage { hash } => {
+            format!("SHA256 Preimage of {}", hash.to_string())
+        }
+        SatisfiableItem::Hash256Preimage { hash } => {
+            format!("Double-SHA256 Preimage of {}", hash.to_string())
+        }
+        SatisfiableItem::Ripemd160Preimage { hash } => {
+            format!("RIPEMD160 Preimage of {}", hash.to_string())
+        }
+        SatisfiableItem::Hash160Preimage { hash } => {
+            format!("Double-RIPEMD160 Preimage of {}", hash.to_string())
+        }
+        SatisfiableItem::AbsoluteTimelock { value } => {
+            format!("Absolute Timelock of {}", value.to_string())
+        }
+        SatisfiableItem::RelativeTimelock { value } => {
+            format!("Relative Timelock of {}", value.to_string())
+        }
+        SatisfiableItem::Multisig { keys, threshold } => {
+            format!("{} of {} MultiSig:", threshold, keys.len())
+        }
+        SatisfiableItem::Thresh { items, threshold } => {
+            format!("{} of {} Threshold:", threshold, items.len())
+        }
+    }
+}
+
+pub struct DisplayKey<'a>(&'a PkOrF);
+
+impl<'a> Display for DisplayKey<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", display_key(self.0))
+    }
+}
+
+fn display_key(key: &PkOrF) -> String {
+    // TODO: Use aliases
+    match key {
+        PkOrF::Pubkey(pk) => format!("<pk:{}>", pk.to_string()),
+        PkOrF::XOnlyPubkey(pk) => format!("<xonly-pk:{}>", pk.to_string()),
+        PkOrF::Fingerprint(f) => format!("<fingerprint:{}>", f.to_string()),
     }
 }
